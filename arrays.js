ef132b77e7f36684663ab2459567e85c2879914e
Alevtina Lobova <ally1125@gmail.com>
2018-01-19 22:51:08 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,46 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
+function addElementToBeginningOfArray(array, element)
+  { return [element,...array]
+}
+addElementToBeginningOfArray(chocolateBars,"payday");
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+destructivelyAddElementToBeginning(chocolateBars,"baby ruth")
+
+function addElementToEndOfArray(array,element){
+  return [...array, element]
+  
+}
+addElementToEndOfArray(chocolateBars,"payday")
+
+function destructivelyAddElementToEndOfArray(array,element){
+  array.push(element)
+  return array
+}
+destructivelyAddElementToEndOfArray(chcolateBars, "payday")
+
+function accessElementInArray(array,index){
+  return array[index]
+}
+accessElementInArray(chocolateBars,0);
+
+const carModels = ["Honda","Toyota","Lexus","BMW"]
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
+destructivelyRemoveElementFromBeginningOfArray(carModels); 
+var cats = ["Mia", "Tulip","Harry"]
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1);
+}
+removeElementFromBeginningOfArray(cats);
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+function removeElementFromEndOfArray(array)
